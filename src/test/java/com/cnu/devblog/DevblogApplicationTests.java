dee1706626f6b0c10d74448f1ae521a840e1a110
inspire12 <ox4443@naver.com>
2023-03-01 03:46:05 +0530

    feat(main) - meta 데이터 정정

--- src/test/java/com/cnu/devblog/DevblogApplicationTests.java
+++ src/test/java/com/cnu/devblog/DevblogApplicationTests.java
@@ -2,12 +2,21 @@ package com.cnu.devblog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class DevblogApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
 	}
 
+	@Test
+	void hasPostControllerConfigured(ApplicationContext context) {
+		assertThat(context.getBean("postController")).isNotNull();
+	}
 }
